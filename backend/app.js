f49a6858e3c164140619425793c551718d41a33b
Khai <mohdkhairin62@gmail.com>
2025-01-26 04:23:04 +0800

    tried cors fix on backend

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,20 @@ require("dotenv").config();
 const colors = require("colors");
 const express = require("express");
 const cors = require("cors");
-const documentRoutes = require('./routes/documents');
+const documentRoutes = require("./routes/documents");
 const app = express();
 const port = 3001;
 
-app.use(cors());
+const corsOptions = {
+  origin: "*.amplifyapp.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.use('/api/documents', documentRoutes);
+app.use("/api/documents", documentRoutes);
 
 app.get("/api/test", (req, res) => {
   console.log("🔵 GET Request to:".blue, "/api/test");
